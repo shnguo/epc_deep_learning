1fa3b6af7b600dd6f972e7699c1d090d2be884fc
郭盛 <shnguo@gmail.com>
2017-09-08 14:30:43 +0800

    908

--- bibi.py
+++ bibi.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
+import os
+import sys
 import pandas as pd #导入Pandas
 import numpy as np #导入Numpy
 import jieba #导入结巴分词
 import keras
+import re
 from keras.preprocessing import sequence
 from keras.optimizers import SGD, RMSprop, Adagrad
 from keras.utils import np_utils
@@ -13,61 +16,55 @@ from keras.layers.recurrent import LSTM, GRU
 from pprint import pprint
 
 Filters=' !"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n'
+pattern = re.compile(r'[a-z0-9]',re.I)
 
-if __name__=='__main__':
-    text = []
-    label = []
-    labels = []
-    with open('case-data.txt') as f:
-        for line in f:
-            templine = line.strip('\n').split('||')
-            text.append(templine[3]+' '+templine[6])
-            label.append([int(templine[1])])
-            labels.append(int(templine[1]))
-    ll = len(set(labels))
-    print(ll)
-    print(len(text))
-    print(len(label))
-    # text = text[:100]
-    # label = label[:100]
-    af_label = keras.utils.to_categorical(label, num_classes=13)
-    text_list = []
-    for _t in text:
-        tmp_list = list(jieba.cut(_t,HMM=False))
-        tmp_list = list(filter(lambda x: x!='' and x not in Filters and x is not None, tmp_list))
-        text_list.append(tmp_list)
-    # d2v_train = pd.DataFrame(text_list)
-    # # print(d2v_train.head())
-    w = []
-    for  _series in text_list:
-        w.extend(_series)
-    w = list(set(w))
-    w = sorted(w)
-    wlen = len(w)
-    dic = {}
-    # pprint(w)
-    for i in range(wlen):
-        dic[w[i]]=i+1
+def get_data(filename):
+    df = pd.read_csv(filename)
+    textlist = []
+    labellist = []
+    for index, row in df.iterrows():
+        istr = ''
+        if isinstance(row[4], str):
+            istr += row[4]
+        if isinstance(row[7], str):
+            istr += row[7]
+        if not istr:
+            print('error')
+            print(row[0])
+            exit()
+        textlist.append(istr)       
+        labellist.append(int(row[2]))
+    return textlist, labellist
 
-    # dic = pd.DataFrame(pd.Series(w).value_counts())
-    # dic.sort_index()
-    # dic['id']=list(range(1,len(dic)+1))
-    # pprint(dic.head())
-    text_num_list = []
-    for _textlist in text_list:
-        text_num_list.append(list(map(lambda x:dic[x], _textlist)))
-    af_text_num_list = list(sequence.pad_sequences(text_num_list, maxlen=50))
-    x = np.array(af_text_num_list)
-    y = af_label
-    xx = x[:-10]
-    yy = y[:-10]
-    xxx = x[-10:]
-    yyy = y[-10:]
-    pprint(x[:5])
-    pprint(y[:5])
-    print('Build model...')
+def get_text_array(textlist):
+    text_array = []
+    for _t in textlist:
+        _tlist = list(jieba.cut(_t))
+        _tlist = list(filter(lambda x: x and x not in Filters and not pattern.findall(x), _tlist))
+        text_array.append(_tlist)
+    return text_array
+
+def get_text_num_map(text_array):
+    _textlist = []
+    text_num_map = {}
+    for _list in text_array:
+        _textlist.extend(_list)
+    _textlist = list(set(_textlist))
+    _textlist.sort()
+    length = len(_textlist)
+    for i in range(0,length):
+        text_num_map[_textlist[i]] = i+1
+    return text_num_map
+
+def get_num_array(text_array, text_num_map):
+    numpy_array = []
+    for _list in text_array:
+        numpy_array.append(list(map(lambda x:text_num_map[x], _list)))
+    return numpy_array
+
+def get_lstm_model(length):
     model = Sequential()
-    model.add(Embedding(len(dic)+1, 256))
+    model.add(Embedding(length, 256))
     model.add(LSTM(256,return_sequences=True))
     model.add(Dropout(0.5))
     model.add(LSTM(128))
@@ -77,9 +74,36 @@ if __name__=='__main__':
     model.compile(loss='categorical_crossentropy',
               optimizer=sgd,
               metrics=['accuracy'])
+    return model
+
+def main():
     model.fit(xx, yy, batch_size=16, nb_epoch=10)
     score = model.evaluate(xxx, yyy, batch_size=16)
     print(score)
+
+
+if __name__=='__main__':
+    filename = 'case_full.csv'
+    textlist, labellist = get_data(filename)
+    text_array = get_text_array(textlist)
+    text_num_map = get_text_num_map(text_array)
+    numpy_array = get_num_array(text_array, text_num_map)
+    numpy_array = list(sequence.pad_sequences(numpy_array, maxlen=50))
+    train_x = np.array(numpy_array)
+    train_y = keras.utils.to_categorical(labellist, num_classes=13)
+    pprint(train_x[:2])
+    pprint(train_y[:2])
+    x1 = train_x[::2]
+    y1 = train_y[::2]
+    x2 = train_x[1::2]
+    y2 = train_y[1::2]
+    llen = len(text_num_map)+1
+    model = get_lstm_model(llen)
+    model.fit(x1, y1, batch_size=16, nb_epoch=10)
+    score = model.evaluate(x2, y2, batch_size=16)
+    print(score)
+
+    
 
 
 
